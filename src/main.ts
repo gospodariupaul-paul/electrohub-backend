a43dcbb4c3f3b2bc4f48fff79f42bfb0128f09bb
Paul-Stelian <paulstelian@example.com>
2026-02-05 19:52:56 +0200

    test db connection

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
 
 async function bootstrap() {
-  console.log("🔥🔥🔥 APP STARTED WITH NEW CODE 🔥🔥🔥");
+  // Test DB connection
+  const prisma = new PrismaClient();
+  try {
+    await prisma.$connect();
+    console.log("🔥 Connected to DB successfully!");
+  } catch (err) {
+    console.error("❌ Failed to connect to DB:", err);
+  }
 
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: '*',
+    methods: 'GET,POST,PATCH,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
   });
 
   await app.listen(3000);
+  console.log("🔥🔥🔥 APP STARTED WITH NEW CODE 🔥🔥🔥");
 }
+
 bootstrap();
