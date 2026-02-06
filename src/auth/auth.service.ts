03cabaf08d5114a924a769f4bbc051dccedfa507
Paul-Stelian <paulstelian@example.com>
2026-02-06 21:27:35 +0200

    Fix double import typo in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
+
 import * as bcrypt from 'bcryptjs';
 
 @Injectable()
