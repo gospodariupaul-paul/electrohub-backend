db385b1942946fb43cb8496fed1424a34a652275
Paul-Stelian <paulstelian@example.com>
2026-02-08 05:54:46 +0200

    Fix users/me by using req.user.sub

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,32 +7,32 @@ import {
   UseGuards,
   Req,
 } from '@nestjs/common';
-
 import { UsersService } from './users.service';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ChangePasswordDto } from './dto/change-password.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  // GET /users/me
   @UseGuards(JwtAuthGuard)
   @Get('me')
   getMe(@Req() req: any) {
-    return this.usersService.findById(req.user.userId);
+    return this.usersService.findById(req.user.sub);
   }
 
+  // GET /users/:id
   @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findById(+id);
   }
 
+  // PATCH /users/change-password
   @UseGuards(JwtAuthGuard)
   @Patch('change-password')
-  changePassword(
-    @Req() req: any,
-    @Body() dto: { oldPassword: string; newPassword: string },
-  ) {
-    return this.usersService.changePassword(req.user.userId, dto);
+  changePassword(@Req() req: any, @Body() dto: ChangePasswordDto) {
+    return this.usersService.changePassword(req.user.sub, dto);
   }
 }
