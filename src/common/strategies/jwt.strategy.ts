a4add5243178f586f63e01affcfd5e1237023097
Paul-Stelian <paulstelian@example.com>
2026-02-08 16:15:55 +0200

    Fix JwtStrategy import path

--- src/common/strategies/jwt.strategy.ts
+++ src/common/strategies/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtStrategy } from './common/strategy/jwt.strategy';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
