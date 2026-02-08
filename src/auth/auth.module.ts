e295bba2581606ecf6d4f7bda4dbe7ad48ef014f
Paul-Stelian <paulstelian@example.com>
2026-02-08 17:51:31 +0200

    Fix AuthModule imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,19 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module';
-import { JwtStrategy } from '../common/strategies/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
-    UsersModule,
+    PrismaModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '15m' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService],
   exports: [AuthService],
 })
 export class AuthModule {}
