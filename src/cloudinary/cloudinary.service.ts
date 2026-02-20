a531c2884a5fa141d2aef9236cbcea31e571b8e6
Paul-Stelian <paulstelian@example.com>
2026-02-20 18:52:49 +0200

    Fix CloudinaryService config

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -3,6 +3,14 @@ import { v2 as cloudinary } from 'cloudinary';
 
 @Injectable()
 export class CloudinaryService {
+  constructor() {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  }
+
   async uploadImage(file: Express.Multer.File) {
     return new Promise((resolve, reject) => {
       cloudinary.uploader.upload_stream(
