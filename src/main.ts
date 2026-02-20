26e9536375a4c02be3d3b537d784618267671b68
Paul-Stelian <paulstelian@example.com>
2026-02-20 16:18:49 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'https://electrohub-frontend.vercel.app',
+      'http://localhost:3000'
+    ],
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
