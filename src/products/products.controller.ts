6d26314d640644fa9fa2c46894e26dd2db31c15a
Paul-Stelian <paulstelian@example.com>
2026-02-20 20:31:23 +0200

    Fix products.controller syntax

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,24 +1,61 @@
-@Post('create')
-@UseInterceptors(FileInterceptor('image'))
-async create(
-  @UploadedFile() file: Express.Multer.File,
-  @Body()
-  body: {
-    name: string;
-    price: any;
-    description: string;
-    categoryId: any;
-    stock: any; // 🔥 AICI ERA PROBLEMA
-  },
-) {
-  const { name, price, description, categoryId, stock } = body;
+import {
+  Controller,
+  Post,
+  Body,
+  UseInterceptors,
+  UploadedFile,
+  Get,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
-  return this.productsService.create({
-    name,
-    price: Number(price),
-    description,
-    categoryId: Number(categoryId),
-    stock: Number(stock), // 🔥 ACUM E GARANTAT
-    image: file,
-  });
+@Controller('products')
+export class ProductsController {
+  constructor(
+    private readonly productsService: ProductsService,
+    private readonly cloudinaryService: CloudinaryService,
+  ) {}
+
+  @Post('create')
+  @UseInterceptors(FileInterceptor('image'))
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body()
+    body: {
+      name: string;
+      price: any;
+      description: string;
+      categoryId: any;
+      stock: any;
+    },
+  ) {
+    const { name, price, description, categoryId, stock } = body;
+
+    return this.productsService.create({
+      name,
+      price: Number(price),
+      description,
+      categoryId: Number(categoryId),
+      stock: Number(stock),
+      image: file,
+    });
+  }
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
 }
