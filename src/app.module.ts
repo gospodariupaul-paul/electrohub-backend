bca940b89541e0ffb3d9b95992d490b81cd32103
Paul-Stelian <paulstelian@example.com>
2026-02-26 00:02:32 +0200

    Restore app.module with all modules + chat modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,38 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+
+// Conversații
 import { ConversationController } from './conversation/conversation.controller';
 import { ConversationService } from './conversation/conversation.service';
+
+// Mesaje
 import { MessageController } from './message/message.controller';
 import { MessageService } from './message/message.service';
-import { PrismaService } from './prisma/prisma.service';
+
+// Pusher
 import { PusherService } from './pusher/pusher.service';
 
+// MODULELE TALE ORIGINALE (IMPORTANT!)
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { MailModule } from './mail/mail.module';
+
 @Module({
+  imports: [
+    AuthModule,
+    ProductModule,
+    UserModule,
+    MailModule,
+  ],
   controllers: [
     ConversationController,
     MessageController,
   ],
   providers: [
+    PrismaService,
     ConversationService,
     MessageService,
-    PrismaService,
     PusherService,
   ],
 })
