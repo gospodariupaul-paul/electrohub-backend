32e3815e01ec3bfac061c8afaf280d3c42fb445f
Paul-Stelian <paulstelian@example.com>
2026-02-25 23:49:42 +0200

    Fix Pusher env types with non-null assertion

--- src/pusher/pusher.service.ts
+++ src/pusher/pusher.service.ts
@@ -7,10 +7,10 @@ export class PusherService {
 
   constructor() {
     this.pusher = new Pusher({
-      appId: process.env.PUSHER_APP_ID,
-      key: process.env.PUSHER_KEY,
-      secret: process.env.PUSHER_SECRET,
-      cluster: process.env.PUSHER_CLUSTER,
+      appId: process.env.PUSHER_APP_ID!,
+      key: process.env.PUSHER_KEY!,
+      secret: process.env.PUSHER_SECRET!,
+      cluster: process.env.PUSHER_CLUSTER!,
       useTLS: true,
     });
   }
