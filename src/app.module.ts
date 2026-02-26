04726bd45f30b4adf969d69cad65fa68d83e14c5
Paul-Stelian <paulstelian@example.com>
2026-02-26 17:15:12 +0200

    fix imports FINAL REAL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 
-// Auth
 import { AuthModule } from './auth/auth.module';
-
-// Users
 import { UsersModule } from './users/users.module';
-
-// Products
 import { ProductsModule } from './products/products.module';
-
-// Categories
 import { CategoriesModule } from './categories/categories.module';
-
-// Orders
 import { OrdersModule } from './orders/orders.module';
 
-// Conversations
 import { ConversationModule } from './conversation/conversation.module';
-
-// Messages
 import { MessageModule } from './message/message.module';
 
-// Pusher
 import { PusherService } from './pusher/pusher.service';
 
 @Module({
