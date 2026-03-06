26c83a435ec9c5352de1538fe17e8462c63d546e
Paul-Stelian <paulstelian@example.com>
2026-03-06 21:47:51 +0200

    remove cookie-parser and fix jwt cookie extractor

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(cookieParser()); // 🔥 OBLIGATORIU
 
   app.enableCors({
     origin: [
