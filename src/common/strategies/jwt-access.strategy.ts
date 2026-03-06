f7bd6967a5fb9eb7a01a93c7331184e290705fc7
Paul-Stelian <paulstelian@example.com>
2026-03-06 21:16:03 +0200

    enable cookie parsing and jwt cookie strategy

--- src/common/strategies/jwt-access.strategy.ts
+++ src/common/strategies/jwt-access.strategy.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
 
 @Injectable()
-export class JwtAccessStrategy extends PassportStrategy(
-  Strategy,
-  'jwt-access',
-) {
+export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => req?.cookies?.jwt || null,
+      ]),
+      ignoreExpiration: false,
       secretOrKey: process.env.JWT_ACCESS_SECRET,
     });
   }
 
-  validate(payload: any) {
-    return payload; // { sub: userId, email: string }
+  async validate(payload: any) {
+    return {
+      id: payload.sub,
+      email: payload.email,
+      role: payload.role,
+    };
   }
 }
