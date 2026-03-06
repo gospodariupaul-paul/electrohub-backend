eeb4d4a6ce17fe51def03fcb1f57a0a499afc1fd
Paul-Stelian <paulstelian@example.com>
2026-03-06 22:56:31 +0200

    fix: register JwtStrategy correctly to enable cookie-based auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,20 +4,21 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-import { JwtAccessStrategy } from '../common/strategies/jwt-access.strategy';
+import { JwtStrategy } from '../common/strategies/jwt.strategy';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule,
     PrismaModule,
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_ACCESS_SECRET,
       signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtAccessStrategy],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
