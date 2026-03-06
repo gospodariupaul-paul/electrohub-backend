76463846c78c2c4acebc7073a69f0f905045ad11
Paul-Stelian <paulstelian@example.com>
2026-03-06 22:11:15 +0200

    fix passport injection in products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,11 +3,13 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
-    PrismaModule,       // 🔥 OBLIGATORIU
-    CloudinaryModule,   // pentru upload imagini
+    PrismaModule,       // acces DB
+    CloudinaryModule,   // upload imagini
+    PassportModule,     // 🔥 OBLIGATORIU pentru req.user
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
