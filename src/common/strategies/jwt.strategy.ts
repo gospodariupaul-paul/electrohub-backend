85550fd639ff4f4c3497bbef9be7a384d55bec56
Paul-Stelian <paulstelian@example.com>
2026-03-06 20:40:33 +0200

    fix jwt cookie strategy

--- src/common/strategies/jwt.strategy.ts
+++ src/common/strategies/jwt.strategy.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
+  constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          return req?.cookies?.jwt || null; // 🔥 token din cookie
+        },
+      ]),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_ACCESS_SECRET'), // 🔥 FIX FINAL
+      secretOrKey: process.env.JWT_ACCESS_SECRET,
     });
   }
 
