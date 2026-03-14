4ec55b8c74c4d5363b2265c4fb07fde8376d7997
Paul-Stelian <paulstelian@example.com>
2026-03-14 12:43:31 +0200

    Fix UpdateSettingsDto validation and fields

--- src/notification/dto/update-settings.dto.ts
+++ src/notification/dto/update-settings.dto.ts
@@ -1,7 +1,23 @@
+import { IsOptional, IsBoolean } from "class-validator";
+
 export class UpdateSettingsDto {
-  email?: boolean;
-  push?: boolean;
-  newProducts?: boolean;
-  messages?: boolean;
-  priceAlerts?: boolean;
+  @IsOptional()
+  @IsBoolean()
+  email_notifications?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  push_notifications?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  product_alerts?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  message_alerts?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  price_alerts?: boolean;
 }
