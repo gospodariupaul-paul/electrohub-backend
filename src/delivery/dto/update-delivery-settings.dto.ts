686759898bc077fd3cad4e39a27b574625b1a695
Paul-Stelian <paulstelian@example.com>
2026-04-05 21:30:26 +0300

    Fix DTO: campuri optionale + validare corecta pentru delivery settings

--- src/delivery/dto/update-delivery-settings.dto.ts
+++ src/delivery/dto/update-delivery-settings.dto.ts
@@ -1,13 +1,47 @@
+import { IsOptional, IsString, IsBoolean } from 'class-validator';
+
 export class UpdateDeliverySettingsDto {
+  @IsOptional()
+  @IsString()
   preferredCourier?: string;
+
+  @IsOptional()
+  @IsString()
   preferredMethod?: string;
+
+  @IsOptional()
+  @IsString()
   street?: string;
+
+  @IsOptional()
+  @IsString()
   number?: string;
+
+  @IsOptional()
+  @IsString()
   city?: string;
+
+  @IsOptional()
+  @IsString()
   county?: string;
+
+  @IsOptional()
+  @IsString()
   postalCode?: string;
+
+  @IsOptional()
+  @IsBoolean()
   callBefore?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
   noSaturday?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
   cashOnDelivery?: boolean;
+
+  @IsOptional()
+  @IsString()
   easyboxId?: string;
 }
